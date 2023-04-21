da16f140570d0be15d62723cad2c640f678e0335
Edwin A. Espinal Taveras <espinaledwin1919@gmail.com>
2023-04-21 15:35:42 -0400

    Continue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,41 @@
 //const owner_id = "843ed68e-12fe-4634-afcd-6ec089f213fc";
+import { useState } from "react";
+import es from "date-fns/locale/es";
+import { DatePickerCalendar } from "react-nice-dates";
 
+import "react-nice-dates/build/style.css";
+import { format, getDay, parse } from "date-fns";
+import { Modal } from "@/components/Modal";
 export default function Home() {
-  return <div className='w-fullh h-screen bg-black text-white'>Home</div>;
+  const [date, setDate] = useState();
+
+  const modifiers = {
+    disabled: (date) => getDay(date) === 0,
+  };
+
+  const handleChangeDate = (e) => {
+    const dateFormat = format(e, "dd/MM/yyyy", { locale: es });
+    setDate(e);
+  };
+
+  return (
+    <div className='w-full h-screen bg-white text-white'>
+      Home
+      <div className=''>
+        <p className='text-black'>
+          Selected date:{" "}
+          {date ? format(date, "dd MMMM yyyy", { locale: es }) : "none"}.
+        </p>
+        <DatePickerCalendar
+          date={date}
+          onDateChange={handleChangeDate}
+          locale={es}
+          modifiers={modifiers}
+        />
+      </div>
+      <div className='bg-black'>
+        <Modal />
+      </div>
+    </div>
+  );
 }
